df30e4363fb665458127465f1125e461de1675c5
Guilherme de Almeida do Carmo <55238625+guilhermealcar@users.noreply.github.com>
2022-06-07 16:26:48 -0300

    atv7

--- PDS_atv7.m
+++ PDS_atv7.m
@@ -27,7 +27,7 @@ F2 = abs(fft(y_filtrado));
 
 figure
 subplot(2,2,1)
-plot(Y)
+plot(noisySignal)
 title('Original Signal')
 xlabel('Time - (s)')
 ylabel('Amplitude')
@@ -55,11 +55,42 @@ ylabel('Magnitude')
 
 load audio_gravado.mat
 
+Nveg = length(Y);
+Fveg = abs(fft(Y));
+Niveg = 0:(N-1)/2;
+omegaveg = Niveg*FS/Nveg;
+
 figure
-subplot(2,1,1)
+subplot(3,2,1)
 plot(Y)
 title('Original Signal')
 
-subplot(2,1,2)
+subplot(3,2,2)
+plot(omegaveg,Fveg(1:end/2))
+
+% Recorded audio file filtered
+Y_filtrado = filter(Hdveg1, Y);
+Y_filtradoTransform = abs(fft(Y_filtrado));
+
+Fveg2 = abs(fft(Y_filtrado));
+
+p3 = audioplayer(Y_filtrado,FS);
+
+subplot(3,2,3)
 plot(Y_filtrado)
+title('Filtered Signal')
+
+subplot(3,2,4)
+plot(omegaveg,Fveg2(1:end/2))
+
+% Recorded audio file filtered - 2
+Y_filtrado2 = filter(Hdveg2, Y_filtrado);
+Y_filtradoTransform2 = abs(fft(Y_filtrado2));
+
+Fveg3 = abs(fft(Y_filtrado2));
+
+p4 = audioplayer(Y_filtrado2,FS);
+
+subplot(3,2,6)
+plot(omegaveg,Fveg3(1:end/2))
 title('Filtered Signal')
