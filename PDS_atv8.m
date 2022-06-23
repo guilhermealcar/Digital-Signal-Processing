f9ebedb84de664fcc36a5ea5f300ce009776ffc9
Guilherme de Almeida do Carmo <55238625+guilhermealcar@users.noreply.github.com>
2022-06-23 16:38:11 -0300

    atv8

--- PDS_atv8.m
+++ PDS_atv8.m
@@ -2,14 +2,12 @@ close all
 clear
 clc
 
-
-
+%%
 load dado_bruto_-_com_rotulo_de_tempo.mat
 
 N = length(t);          % Amostra = 1000
 Fs = 1000;              % Freq de amostragem dada
 xfreq = abs(fft(x));
-
 
 Ni = 0:(N-1)/2;
 omega = Ni*Fs/N;
@@ -40,13 +38,14 @@ N2 = 4.6*pi/deltaW;      % N com janela Blackman
 w2 = window(@blackman, N2);
 
 stem(w2)
-
+%%
 % Para ordem par
 n = -N2/2:N2/2;
 h = sinc(wc * n/pi) * wc/pi;
 
 stem(h)
 
+%%
 b = w2.*h';
 stem(b)
 
@@ -54,7 +53,20 @@ stem(b)
 fvtool(b,1);
 xfilt = conv(x,b,"same");
 
+%%
 figure
 hold all
 plot(x)
 plot(xfilt)
+legend('Original Signal', 'Filtered Signal')
+xlabel('Time - (s)')
+ylabel('Amplitude')
+
+%%
+xfreqFilt = abs(fft(xfilt));
+
+figure
+stem(xfreqFilt(1:end/2))
+title('Filtered Signal')
+xlabel('Frequency - (Hz)')
+ylabel('Magnitude')
