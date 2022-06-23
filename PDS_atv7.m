1916d11f7974e1c9ed21b3b1c3d9f0b20f040420
Guilherme de Almeida do Carmo <55238625+guilhermealcar@users.noreply.github.com>
2022-06-22 23:39:29 -0300

    Update PDS_atv7.m

--- PDS_atv7.m
+++ PDS_atv7.m
@@ -27,7 +27,7 @@ F2 = abs(fft(y_filtrado));
 
 figure
 subplot(2,2,1)
-plot(Y)
+plot(noisySignal)
 title('Original Signal')
 xlabel('Time - (s)')
 ylabel('Amplitude')
@@ -55,11 +55,62 @@ ylabel('Magnitude')
 
 load audio_gravado.mat
 
+Nveg = length(Y);
+Fveg = abs(fft(Y));
+Niveg = 0:(N-1)/2;
+omegaveg = Niveg*FS/Nveg;
+
 figure
-subplot(2,1,1)
+subplot(3,2,1)
 plot(Y)
 title('Original Signal')
+xlabel('Time - (s)')
+ylabel('Amplitude')
 
-subplot(2,1,2)
+subplot(3,2,2)
+plot(omegaveg,Fveg(1:end/2))
+title('Original Signal')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+
+% Recorded audio file filtered
+Y_filtrado = filter(Hdveg1, Y);
+Y_filtradoTransform = abs(fft(Y_filtrado));
+
+Fveg2 = abs(fft(Y_filtrado));
+
+p3 = audioplayer(Y_filtrado,FS);
+
+subplot(3,2,3)
 plot(Y_filtrado)
 title('Filtered Signal')
+xlabel('Time - (s)')
+ylabel('Amplitude')
+
+subplot(3,2,4)
+plot(omegaveg,Fveg2(1:end/2))
+title('Stopband Filtered Signal')
+subtitle('first stopband (object Hdveg1)')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
+
+% Recorded audio file filtered - 2
+Y_filtrado2 = filter(Hdveg2, Y_filtrado);
+Y_filtradoTransform2 = abs(fft(Y_filtrado2));
+
+Fveg3 = abs(fft(Y_filtrado2));
+
+p4 = audioplayer(Y_filtrado2,FS);
+
+subplot(3,2,5)
+plot(Y_filtrado2)
+title('Filtered Signal')
+xlabel('Time - (s)')
+ylabel('Amplitude')
+
+subplot(3,2,6)
+plot(omegaveg,Fveg3(1:end/2))
+title('Stopband Filtered Signal')
+subtitle('second stopband (object Hdveg2)')
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
